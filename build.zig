eac6b48464e25d6ad28353a538e9ffce7b3b5654
LordMZTE <lord@mzte.de>
2023-04-02 20:33:44 +0200

    use Zig build system

---
+++ build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const freetype = b.option(bool, "freetype", "use freetype") orelse true;
+    // TODO: somehow let freetype choose the default value here?
+    const freetype_brotli = b.option(bool, "freetype_brotli", "Makes the freetype dependency use brotli") orelse false;
+
+    const lib = b.addStaticLibrary(.{
+        .name = "harfbuzz",
+        .root_source_file = .{ .path = "src/harfbuzz.cc" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib.linkLibCpp();
+
+    if (freetype) {
+        lib.linkLibrary(b.dependency("freetype", .{
+            .brotli = freetype_brotli,
+        }).artifact("freetype"));
+    }
+
+    lib.install();
+}
