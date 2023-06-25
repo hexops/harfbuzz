cef3a8270472dc215b51a2ef69ea2ed0ee6b7c75
Stephen Gutekanst <stephen@hexops.com>
2023-06-24 18:24:25 -0700

    improve source verification script
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- verify.sh
+++ verify.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git diff $(git merge-base origin/main upstream/main)..origin/main \
+git diff $(git merge-base main upstream/main)..main \
     --diff-filter=d \
     ':(exclude)README.md' \
     ':(exclude)build.zig' \
     ':(exclude)build.zig.zon' \
+    ':(exclude)update.sh' \
+    ':(exclude)verify.sh' \
     ':(exclude).github' \
     ':(exclude).gitignore'
