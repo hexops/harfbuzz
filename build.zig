83493f5f5b7e186fc144c503b80dd0f3cdffb6c3
Ali Chraghi <alichraghi@proton.me>
2023-07-31 14:15:00 +0330

    update build.zig

--- build.zig
+++ build.zig
@@ -3,12 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const use_freetype = b.option(bool, "enable_freetype", "Build Freetype") orelse false;
-    const enable_brotli = b.option(bool, "enable_freetpye_brotli", "Build brotli") orelse false;
+    const enable_freetype = b.option(bool, "enable_freetype", "Build Freetype") orelse false;
+    const use_system_zlib = b.option(bool, "freetype_use_system_zlib", "Use system zlib") orelse false;
+    const enable_brotli = b.option(bool, "freetype_enable_brotli", "Build brotli") orelse false;
 
     const freetype_dep = b.dependency("freetype", .{
         .target = target,
         .optimize = optimize,
+        .use_system_zlib = use_system_zlib,
         .enable_brotli = enable_brotli,
     });
 
@@ -19,12 +21,16 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     lib.linkLibCpp();
-    if (use_freetype) lib.linkLibrary(freetype_dep.artifact("freetype"));
     lib.installHeadersDirectoryOptions(.{
         .source_dir = .{ .path = "src" },
         .install_dir = .header,
         .install_subdir = "harfbuzz",
         .exclude_extensions = &.{".cc"},
     });
+    if (enable_freetype) {
+        lib.defineCMacro("HAVE_FREETYPE", "1");
+        lib.linkLibrary(freetype_dep.artifact("freetype"));
+        lib.installLibraryHeaders(freetype_dep.artifact("freetype"));
+    }
     b.installArtifact(lib);
 }
